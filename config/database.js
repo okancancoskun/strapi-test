0c2fa1d99a4c7012ed2dc4bf3dfacd383994d629
buroz <burakoz@zoho.com>
2022-05-18 11:15:57 +0300

    14

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: env("DATABASE_CLIENT", "postgres"),
+
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'busbud-test-db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '8318913'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
+    debug: false,
   },
 });
